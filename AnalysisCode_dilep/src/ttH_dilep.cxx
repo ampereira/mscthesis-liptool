5782d4b0e6e90ea55a26d28812e03ecd2288c46e
ampereira <lazeroth89@gmail.com>
2013-02-08 17:40:40 +0000

    fixing variances

--- AnalysisCode_dilep/src/ttH_dilep.cxx
+++ AnalysisCode_dilep/src/ttH_dilep.cxx
@@ -4033,7 +4033,7 @@ void ttH_dilep::ttDilepKinFit(){
 									// Find tt dileptonic solutions
 									// ---------------------------------------
 									DilepInput di (z_lep, c_lep, z_bj, c_bj, z_bjWFlags, c_bjWFlags, z_lepWFlags, c_lepWFlags, jet1_HiggsWFlags, jet2_HiggsWFlags, in_mpx, in_mpy, in_mpz, MissPx, MissPy, t_m, w_m);
-									inputs.push_back(di);
+									combos.push_back(di);
 								}
 							}
 						}
@@ -4045,6 +4045,7 @@ void ttH_dilep::ttDilepKinFit(){
 
 	// Apply the variations to the inputs
 	//inputs = applyVariance(combos, RESOLUTION, dilep_iterations, EveNumber + JetVec.size()*100);
+	inputs = combos;
 
 	for (unsigned counter = 0; counter < inputs.size(); ++counter) {
 		DilepInput di = inputs[counter];
