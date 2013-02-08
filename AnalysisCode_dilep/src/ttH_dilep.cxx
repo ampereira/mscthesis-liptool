359853788cf0aed8b4121dfc32f5a11f97f436a4
ampereira <lazeroth89@gmail.com>
2013-02-08 11:14:25 +0000

    fixing the shit I've done

--- AnalysisCode_dilep/src/ttH_dilep.cxx
+++ AnalysisCode_dilep/src/ttH_dilep.cxx
@@ -4030,7 +4030,7 @@ void ttH_dilep::ttDilepKinFit(){
 									// ---------------------------------------
 									// Find tt dileptonic solutions
 									// ---------------------------------------
-									DilepInput di (z_lep, c_lep, z_bj, c_bj, z_bjWFlags, c_bjWFlags, z_lepWFlags, c_lepWFlags, jet1_HiggsWFlags, jet2_HiggsWFlags, in_mpx, in_mpy, in_mpz, MissPx, MissPy, t_m, w_m);
+									//DilepInput di (z_lep, c_lep, z_bj, c_bj, z_bjWFlags, c_bjWFlags, z_lepWFlags, c_lepWFlags, jet1_HiggsWFlags, jet2_HiggsWFlags, in_mpx, in_mpy, in_mpz, MissPx, MissPy, t_m, w_m);
 
 									//di.applyVariance(RESOLUTION, EveNumber + JetVec.size()*100);
 
@@ -4052,7 +4052,7 @@ void ttH_dilep::ttDilepKinFit(){
 
 									HasSolution += partial_sol_count;
 
-
+									/*
 									// Returns the values varied
 									z_lep = di.getZlep();
 									c_lep = di.getClep();
@@ -4071,7 +4071,7 @@ void ttH_dilep::ttDilepKinFit(){
 									in_mpz[0] = di.getInMpz(0);
 									in_mpz[1] = di.getInMpz(1);
 									z_bl = di.getZbl();
-									c_bl = di.getCbl();
+									c_bl = di.getCbl();*/
 
 									// ---------------------------------------
 									// Get info from all possible solutions
