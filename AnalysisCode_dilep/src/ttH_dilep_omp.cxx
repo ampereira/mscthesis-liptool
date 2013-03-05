08869c61a3ad51b8c7699ba73a1f34b30a8b4eb1
ampereira <lazeroth89@gmail.com>
2013-03-05 15:59:00 +0000

    Added ttDKF_Best_sol class for final merging of the best solution

--- AnalysisCode_dilep/src/ttH_dilep_omp.cxx
+++ AnalysisCode_dilep/src/ttH_dilep_omp.cxx
@@ -4080,8 +4080,10 @@ void ttH_dilep::ttDilepKinFit(){
 
 
 	// Best solution merge
-	ttDKF_Best_Sol best_sols [NUM_THREADS];
+	ttDKF_Best_Sol best_sols [num_threads];
 
+
+	omp_set_num_threads(num_threads);
 
 	#pragma omp private(di, result, task_id, nTSol, _ProbHiggs_ttDKF, _ProbTTbar_ttDKF, _ProbTotal_ttDKF, n_ttDKF_Best, \
 	MaxTotalProb, MaxHiggsProb, myttbar_px, myttbar_py, myttbar_pz, myttbar_E, theta_jet1_HiggsFromTTbar, \
@@ -4089,7 +4091,7 @@ void ttH_dilep::ttDilepKinFit(){
 	_b1_ttDKF, _b2_ttDKF, _l1_ttDKF, _l2_ttDKF, _W1_ttDKF, _W2_ttDKF, _t1_ttDKF, _t2_ttDKF, _ttbar_ttDKF, \
 	_b1_Higgs_ttDKF, _b2_Higgs_ttDKF, _Higgs_ttDKF, _mHiggsJet1_ttDKF, _mHiggsJet2_ttDKF)
 
-	#pragma omp parallel num_threads(NUM_THREADS)
+	#pragma omp parallel
 	{
 	#pragma omp parallel for reduction(+:HasSolution_private)
 	for (unsigned counter = 0; counter < inputs.size() * dilep_iterations; ++counter) {
