fd2eb7fd81250aa0b7cf2d1dd0c2fb400657b233
ampereira <lazeroth89@gmail.com>
2013-04-26 13:05:01 +0100

    trying to get cuda to work

--- AnalysisCode_dilep/src/ttH_dilep_cuda.cu
+++ AnalysisCode_dilep/src/ttH_dilep_cuda.cu
@@ -4095,6 +4095,10 @@ void ttH_dilep::ttDilepKinFit(){
 
 		std::vector<myvector> result = di.getResult();
 		HasSolution += di.getHasSol();
+
+		ofstream of("result.txt", fstream::app);
+		of << EveNumber << " - " << result.size() << endl;
+		of.close();
 		
 		for ( int id = 0; id < result.size(); id++) {
 		
