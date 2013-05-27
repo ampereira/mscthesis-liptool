cb29b23122dcabeeadb9d9766e1a668ecc1bc373
ampereira <lazeroth89@gmail.com>
2013-05-27 11:29:47 +0100

    Fixing PRNG on GPU

--- AnalysisCode_dilep/src/cuda/neut.cu
+++ AnalysisCode_dilep/src/cuda/neut.cu
@@ -1,5 +1,11 @@
 #include "neut.h"
 
+#include <curand_kernel.h>
+/* include MTGP host helper functions */
+#include <curand_mtgp32_host.h>
+/* include MTGP pre-computed parameter sets */
+#include <curand_mtgp32dc_p_11213.h>
+
 using std::vector;
 using namespace std;
 
