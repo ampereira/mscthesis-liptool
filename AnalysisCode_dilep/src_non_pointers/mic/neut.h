e59eb456cfbb8a874ae40de43747024a19b961c7
ampereira <lazeroth89@gmail.com>
2013-06-19 11:41:04 +0100

    Understanding a bit of the MIC shitz

--- AnalysisCode_dilep/src_non_pointers/mic/neut.h
+++ AnalysisCode_dilep/src_non_pointers/mic/neut.h
@@ -20,6 +20,7 @@
 
 #define STRIDE2(a,i) a[tid * 2 + i]
 #define STRIDE5(a,i) a[tid * 5 + i]
+#define TO1D(nc,tid,sol,did) nc[tid*16+sol*4+did]
 
 namespace Dilep {
 	namespace MIC {
@@ -39,8 +40,8 @@ namespace Dilep {
 		void dilep (DilepInput &di);
 
 		std::vector<myvector>* __attribute__((target(mic))) calc_dilep(double t_mass[], double w_mass[], 
-										double in_mpx[], double in_mpy[], double in_mpz[],
-										double _lep_a[], double _lep_b[], 
-										double _bl_a[], double _bl_b[], int tid);
+													double in_mpx[], double in_mpy[], double _lep_a[], 
+													double _lep_b[], double _bl_a[], double _bl_b[], 
+													double nc[], int a[], int tid);
 	}
 }
