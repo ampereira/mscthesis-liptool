9d46e713c4d071c3ac33fc1fc4c3f5ad27c8a79c
ampereira <lazeroth89@gmail.com>
2013-07-02 17:39:53 +0100

    asd

--- AnalysisCode_dilep/src_non_pointers/mic/calc.cxx
+++ AnalysisCode_dilep/src_non_pointers/mic/calc.cxx
@@ -31,7 +31,7 @@ void calc_dilep_mic(double t_mass[], double w_mass[],
 	int size5 = size * 5;
 	int size16 = size * 16;
 
-	#pragma offload target(mic) in(w_mass,t_mass,in_mpx,in_mpy:length(size2)) in(_lep_a,_lep_b,_bl_a,_bl_b:length(size5)) out(nc:length(size16)) out(a:length(size))
+	#pragma offload target(mic:0) in(w_mass,t_mass,in_mpx,in_mpy:length(size2)) in(_lep_a,_lep_b,_bl_a,_bl_b:length(size5)) out(nc:length(size16)) out(a:length(size))
 	{
 //		#pragma omp parallel
 		{
