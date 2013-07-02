a06739f27db3f582605b1db80ddd018b98c1806f
ampereira <lazeroth89@gmail.com>
2013-07-02 16:21:07 +0100

    asd

--- AnalysisCode_dilep/src_non_pointers/mic/calc.h
+++ AnalysisCode_dilep/src_non_pointers/mic/calc.h
@@ -11,7 +11,7 @@ inline void __attribute__((target(mic))) toz(double k[], double l[], double g[])
 inline void __attribute__((target(mic))) my_qu( double my_in[], double my_val[]);
 inline double __attribute__((target(mic))) calcMass(double x, double y, double z, double e);
 
-inline void __attribute__((target(mic))) calc_dilep(double t_mass[], double w_mass[], 
+inline void __attribute__((target(mic))) calc_dilep_mic(double t_mass[], double w_mass[], 
 											double in_mpx[], double in_mpy[], double _lep_a[], 
 											double _lep_b[], double _bl_a[], double _bl_b[], 
 											double nc[], int a[], int size);
