3ce7fb79a18422c489f8540cb019805bd442bd16
ampereira <lazeroth89@gmail.com>
2013-07-11 15:45:43 +0100

    FUNCIONA SEM VARIACOES TUDO EM GPU

--- AnalysisCode_dilep/scheduler/scheduler.cpp
+++ AnalysisCode_dilep/scheduler/scheduler.cpp
@@ -20,7 +20,7 @@ int main (void) {
 	string inputs ("--OutputFileName=ttH125_dilepbb_em --SetSystematicsFileName=../../RefSys/Ref.txt --Sample=901 --User=\"CutTriggerEleMuo=1\" --User=\"lepSample=23\"");
 
 
-	vector<App> applications (num_total_runs);
+	vector<App> applications;
 
 	// build apps vector
 	for (int i = 0; i < num_total_runs; ++i) {
