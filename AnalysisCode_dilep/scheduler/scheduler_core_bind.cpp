35a755054c03ce15d67756f65991cf9f520e1db6
ampereira <lazeroth89@gmail.com>
2014-02-07 11:42:40 +0000

    ldsa

--- AnalysisCode_dilep/scheduler/scheduler_core_bind.cpp
+++ AnalysisCode_dilep/scheduler/scheduler_core_bind.cpp
@@ -107,11 +107,12 @@ void readInputs (int argc, char **argv) {
 // What each thread will execute
 void* worker (void *ptr) {
 	int ret;
-	unsigned id = (unsigned) *ptr;
+	unsigned id;
+	&id = (unsigned*) *ptr;
 
 	cpu_set_t cpuset;
 	CPU_ZERO(&cpuset);
-	CPU_SET(, &cpuset);
+	CPU_SET(id, &cpuset);
 	pthread_t self = pthread_self();
 	pthread_setaffinity_np(self, sizeof(cpu_set_t), &cpuset);
 
